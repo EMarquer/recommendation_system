61f92f8fa60700bb7377a0fc159c2f15b3af870c
Maxime GUILLAUME <maxime.guillaume2@etu.univ-lorraine.fr>
2020-02-06 01:48:41 +0100

    Update gui

--- app/main.py
+++ app/main.py
@@ -3,8 +3,11 @@ import numpy as np
 import pandas as pd
 import time
 import svd
+import ranking
+import numpy as np
 
-
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (7,4)
 
 # CONSTANTS
 SEASONS = ('Automne', 'Hiver', 'Éte','Printemps')
@@ -12,17 +15,28 @@ SEASONS = ('Automne', 'Hiver', 'Éte','Printemps')
 TITLE = "Recommandation"
 SIDE_TITLE = "Paramètres"
 
+
+
+
 SCORE_FILTERING_MESSAGE = "Filtrer par le score moyen des avis :"
 LOCATION_MESSAGE = "Entrez votre position :"
-SEASON_DEPARTURE_MESSAGE = "Quand souhaitez-vous partir ?"
-IDENTIFIER_MESSAGE="Taper votre identifiant : "
-RECENT_REVIEW_MESSAGE = "Je souhaite privilégier les avis récents."
-ACCOUNT_SENT_ANALYSIS_MESSAGE = "Je souhaite utiliser la valence émotionelle du contenu des avis."
-HISTORY_MESSAGE = "Je souhaite utiliser mon historique."
+SEASON_DEPARTURE_MESSAGE = "Entrez la période du voyage :"
+IDENTIFIER_MESSAGE="Entrez votre identifiant : "
+RECENT_REVIEW_MESSAGE = "Privilégier les avis récents."
+ACCOUNT_SENT_ANALYSIS_MESSAGE = "Mettre en avant les avis au contenu positif"
+HISTORY_MESSAGE = "Utiliser mon historique."
 LOADING_MESSAGE = "Patientez un instant."
-DETAILS_MESSAGE = "Je souhaite voir les détails."
+DETAILS_MESSAGE = "Afficher les détails."
 
 LABEL_BUTTON_SEARCH = "Rechercher"
+
+
+#Preloading
+df_data = pd.read_csv("data/processed_data.csv.zip")
+
+df_data["stay_date"] = pd.to_datetime(df_data["stay_date"])
+df_data["gap_stay_today"] = pd.to_timedelta(df_data["gap_stay_today"])
+
 
 # UI
 st.title(TITLE)
@@ -35,20 +49,19 @@ review_score_threshold = st.sidebar.slider(SCORE_FILTERING_MESSAGE,
                                            step=1)
 # User location
 user_location = st.sidebar.text_input(LOCATION_MESSAGE)
+user_location = user_location if user_location else None
 
 # Season
 season = st.sidebar.multiselect(SEASON_DEPARTURE_MESSAGE,SEASONS)
-
+season = season if season else None
 
 use_history = st.sidebar.checkbox(HISTORY_MESSAGE)
 
+user_id = None
 if use_history:
     
 # Identifier (We should apply less 1 to have the correct id )
-    user_id = st.sidebar.number_input(
-        IDENTIFIER_MESSAGE,
-        step = 1,min_value=0,value=0
-    )
+    user_id = st.sidebar.text_input(IDENTIFIER_MESSAGE)
 
 # Checkboxes
 prioritize = st.sidebar.checkbox(RECENT_REVIEW_MESSAGE)
@@ -57,19 +70,64 @@ more_details= st.sidebar.checkbox(DETAILS_MESSAGE)
 
 
 
+
+AVERAGE_SCORE = "score"
+
+
+
+def recommand(n=20,alpha = 1 ,beta = 1):
+   
+    df = ranking.rank_from_scores(df_data,
+                                         min_score = review_score_threshold,
+                                         recency = prioritize,
+                                         positivity = sent_analysis,
+                                         season=season,
+                                         city = user_location)
+    df = pd.DataFrame(df)
+
+    
+    if user_id: 
+        df_svd= svd.predict(user_id)
+        df = df.join(df_svd, lsuffix='_caller', rsuffix='_other')
+        df[AVERAGE_SCORE] = (alpha  * df[ranking.GLOBAL_SCORES ]) +  (beta* df[svd.SCORE_SVD])
+        df[AVERAGE_SCORE] = df[AVERAGE_SCORE]/(alpha+beta)
+
+        return df.nlargest(n, AVERAGE_SCORE, keep='first')
+
+    # st.table(df[ranking.GLOBAL_SCORES ])
+    return df.nlargest(n,ranking.GLOBAL_SCORES, keep='first')
+    
 def recommand_ui():
-    res = recommand()
-    if not res:
-        df = pd.DataFrame()
-        df['hotel'] = [543]
-        df['score'] = [4]
-        st.table(df)
-    else:
-        st.table(res)
+
+
+    def row_to_gui(top_k,detail):
+
+
+        col_sel = [ranking.GLOBAL_SCORES]
+        
+        if detail:
+            col_sel = top_k.columns.difference([ranking.RECENCY_SCORES,ranking.POSITIVITY_SCORES])
+        
+        if user_id:
+            col_sel.extend([svd.SCORE_SVD,AVERAGE_SCORE])
 
                     
-def recommand(): 
-    pass
+        for index, row in top_k.iterrows():
+            st.table(row[col_sel])
+
+            if detail:
+                plt.hist(row[ranking.RECENCY_SCORES])
+                plt.title("Distribution de la récence")
+                st.pyplot()
+
+                plt.hist(row[ranking.POSITIVITY_SCORES ])
+                plt.title("Distribution des scores de positivité")
+                st.pyplot()
+                
+    top_k = recommand() 
+    row_to_gui(top_k,more_details)
+
+                    
 
 if st.sidebar.button(LABEL_BUTTON_SEARCH):
 
--- app/svd.py
+++ app/svd.py
@@ -2,24 +2,37 @@ from joblib import dump, load
 from sklearn import preprocessing
 from surprise import SVD,NMF,SVDpp
 from surprise import NormalPredictor
+import pandas as pd
 
 
 
-hotel_le = load('le_hotel.joblib')
-user_le = load('le_user.joblib')
-model = load('svd.joblib')
+SCORE_SVD = "Score SVD" 
+
+
+
+hotel_le = load('app/le_hotel.joblib')
+user_le = load('app/le_user.joblib')
+model = load('app/svd.joblib')
 
 # List of hotels
-hotels = hotel_le.transform(hotel_le.classes_)
+hotel_list = hotel_le.transform(hotel_le.classes_)
 
-
-def predict_top_k(user='Anon',k=10):
+def predict(user='Anon'):
     user_id = user_le.transform([user])[0]
     predictions = []
-    for h in hotels:
+
+    for h in hotel_list:
         predictions.append((h,model.predict(user_id,h)[3]))    
     predictions.sort(key=lambda x: x[1], reverse=True)
-    just_hotel = [p[0] for p in predictions[0:k]]
-    return hotel_le.inverse_transform(just_hotel)
 
-#print(predict_top_k('GlennMiller_11'))
+    hotels, scores = zip(*predictions)
+
+    df = pd.DataFrame()
+    #df['hotel_id'] =  hotel_le.inverse_transform(hotels)
+    df['hotel_id'] =  hotels
+    df[SCORE_SVD] = scores
+
+    df = df.set_index('hotel_id')
+    return df
+
+print(predict('GlennMiller_11'))
